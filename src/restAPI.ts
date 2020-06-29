ffa58e299f321a0f15391480b1f9ef7a032b8252
Pingoin <p.drente@gmx.de>
2020-06-29 22:50:19 +0200

    ejs-lib import von https://www.jsdelivr.com/

--- src/restAPI.ts
+++ src/restAPI.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
-import {Dokus} from "./docus";
-import { promises as fileSystem} from "fs";
+import { Dokus } from "./docus";
+import { promises as fileSystem } from "fs";
 import path from "path";
 
 class RestApi {
     public express: express.Application;
-    private docuDB:Dokus;
+    private docuDB: Dokus;
     constructor() {
         this.express = express();
         this.middleware();
         this.routes();
-        this.docuDB=new Dokus();
+        this.docuDB = new Dokus();
         this.docuDB.init("N:\\");
     }
     private middleware(): void {
@@ -22,21 +22,21 @@ class RestApi {
 
         this.express.get("/files", (req, res) => {
             console.log(req.query.page);
-            let page=0;
-            if (req.query.page){
-                page=parseInt(req.query.page.toString())-1;
+            let page = 0;
+            if (req.query.page) {
+                page = parseInt(req.query.page.toString()) - 1;
             }
-            this.docuDB.getDokus(25,page).then((resolve=>{
-                fileSystem.readFile(path.normalize(__dirname+"/../com/views/partials/dokuTable.ejs"))
-                .then((html)=>{
-                    resolve.template=html.toString();
-                    res.json(resolve);
-                }).catch((reject)=>{
-                    console.log(reject);
-                    res.send("Template nicht vorhanden: "+req.query.template);
-                });
-            })); 
+            this.docuDB.getDokus(25, page).then((resolve => {
+                fileSystem.readFile(path.normalize(__dirname + "/../com/views/partials/dokuTable.ejs"))
+                    .then((html) => {
+                        resolve.template = html.toString();
+                        res.json(resolve);
+                    }).catch((reject) => {
+                        console.log(reject);
+                        res.send("Template nicht vorhanden: " + req.query.template);
+                    });
+            }));
         });
-    } 
+    }
 }
 export default new RestApi().express;
